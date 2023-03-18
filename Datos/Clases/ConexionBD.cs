198ae2ce108def45c600bed430f302f020b25ad9
ElWillyGod <manuelantognazza@gmail.com>
2023-03-18 11:29:12 -0300

    ConexionBD

--- Datos/Clases/ConexionBD.cs
+++ Datos/Clases/ConexionBD.cs
@@ -6,7 +6,7 @@ namespace Datos
 {
     public class ConexionBD
     {
-        public static string conexion = "datasource = localhost; port = 3306; username = root; password = 08001930WillyGod; persistsecurityinfo = True; SSL Mode = None";
+        public static string conexion = "datasource = localhost; port = 3306; username = User; password = PassUSer; persistsecurityinfo = True; SSL Mode = None";
         //"server = localhost; port = 3306; database = pollux; uid = *user* ; pwd =*passUser*"; string para conectar por si no funciona al anterior
 
         public static MySqlConnection miConexion = new MySqlConnection(conexion);
